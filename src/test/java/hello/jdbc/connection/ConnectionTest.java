e8ecce938f96d13e1379d2a28cbe0ff16f77d3f9
LEE HYO JEONG <hyojeong1913@naver.com>
2022-05-13 00:35:49 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -46,6 +47,37 @@ public class ConnectionTest {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
         useDataSource(dataSource);
+    }
+
+    /**
+     * DataSource Connection Pool 추가
+     *
+     * HikariCP 커넥션 풀을 사용
+     *
+     * 커넥션 풀에서 커넥션을 생성하는 작업은 애플리케이션 실행 속도에 영향을 주지 않기 위해 별도의 쓰레드에서 작동
+     *
+     * 별도의 쓰레드에서 동작하기 때문에 테스트가 먼저 종료되어 버리므로
+     * Thread.sleep 을 통해 대기 시간을 주어야 쓰레드 풀에 커넥션이 생성되는 로그를 확인 가능
+     *
+     * @throws SQLException
+     * @throws InterruptedException
+     */
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); // 커넥션 풀 최대 사이즈를 10 으로 지정
+        dataSource.setPoolName("MyPool"); // 풀의 이름을 MyPool 이라고 지정
+
+        useDataSource(dataSource);
+
+        // 커넥션 풀에서 커넥션 생성 시간 대기
+        Thread.sleep(1000);
     }
 
     /**
